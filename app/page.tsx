63e0d95c91cd3aeeaaefd42db1bebdff8170f632
Guilherme Bello <guib.bello@gmail.com>
2026-01-08 20:38:07 -0300

    feat: add appointment panel

--- app/page.tsx
+++ app/page.tsx
@@ -3,16 +3,22 @@ import { Input } from "./_components/ui/input";
 import { Button } from "./_components/ui/button";
 import { SearchIcon } from "lucide-react";
 import Image from "next/image";
+import { Card, CardContent } from "./_components/ui/card";
+import { Badge } from "./_components/ui/badge";
+import { Avatar, AvatarImage } from "./_components/ui/avatar";
 
 const Home = () => {
   return (
     <div>
       {/* Header */}
       <Header />
+
+      {/* Texto */}
       <div className="p-5">
         <h2 className="text-xl font-bold">Olá, Guilherme!</h2>
         <p>Quarta-feira, 7 de Janeiro</p>
 
+        {/* Busca */}
         <div className="mt-6 flex items-center gap-2">
           <Input placeholder="Pesquisar..." />
           <Button>
@@ -20,6 +26,7 @@ const Home = () => {
           </Button>
         </div>
 
+        {/* Imagem */}
         <div className="relative w-full h-[150px] mt-6">
           <Image
             alt="Agende nos melhores com FSW Barber"
@@ -28,6 +35,30 @@ const Home = () => {
             className="object-cover rounded-xl"
           />
         </div>
+
+        {/* Agendamento */}
+        <Card className="mt-6">
+          <CardContent className="flex justify-between p-0">
+            {/* Esquerda */}
+            <div className="flex flex-col gap-2 py-5 pl-5">
+              <Badge className="w-fit">Confirmado</Badge>
+              <h3 className="font-semibold">Corte de Cabelo</h3>
+
+              <div className="flex items-center gap-2">
+                <Avatar className="h-6 w-6">
+                  <AvatarImage src="https://utfs.io/f/c97a2dc9-cf62-468b-a851-bfd2bdde775f-16p.png" />
+                </Avatar>
+                <p className="text-sm">Barbearia FSW</p>
+              </div>
+            </div>
+            {/* Direita */}
+            <div className="flex flex-col items-center justify-center px-5 border-l-2 border-solid">
+              <p className="text-sm">Janeiro</p>
+              <p className="text-2xl">09</p>
+              <p className="text-sm">17:00</p>
+            </div>
+          </CardContent>
+        </Card>
       </div>
     </div>
   );
