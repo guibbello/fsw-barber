df1c2510709ecdc47fd8822af34c1144e2f7e2cf
Guilherme Bello <guib.bello@gmail.com>
2026-01-27 20:44:58 -0300

    feat: ensure booking details can be viewed

---
+++ app/_components/booking-summary.tsx
@@ -0,0 +1,34 @@
+// import { Card, CardContent } from "@/components/ui/card";
+
+// const BookingSummary = () => {
+//   <Card>
+//     <CardContent className="p-3 space-y-3">
+//       <div className="flex justify-between items-center">
+//         <h2 className="font-bold ">{service.name}</h2>
+//         <p className="text-sm font-bold">
+//           {Intl.NumberFormat("pt-BR", {
+//             style: "currency",
+//             currency: "BRL",
+//           }).format(Number(service.price))}
+//         </p>
+//       </div>
+
+//       <div className="flex justify-between items-center">
+//         <h2 className="text-sm text-gray-400">Data</h2>
+//         <p className="text-sm">{selectedDay?.toLocaleDateString("pt-BR")}</p>
+//       </div>
+
+//       <div className="flex justify-between items-center">
+//         <h2 className="text-sm text-gray-400">Horário</h2>
+//         <p className="text-sm">{selectedTime}</p>
+//       </div>
+
+//       <div className="flex justify-between items-center">
+//         <h2 className="text-sm text-gray-400">Barbearia</h2>
+//         <p className="text-sm">{barbershop.name}</p>
+//       </div>
+//     </CardContent>
+//   </Card>;
+// };
+
+// export default BookingSummary;
